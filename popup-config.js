e8556233dcbce3895ee2c5e915086bb9939825ab
venkateshtest19 <venkatesh.mahe.edu@gmail.com>
2026-08-19 17:08:03 +0530

    popup-config.js

--- popup-config.js
+++ popup-config.js
@@ -11,7 +11,7 @@
 */
 
 const popupConfig = {
-  isEnabled: false, // SET TO false TO DISABLE POPUP EVERYWHERE
+  isEnabled: true, // SET TO false TO DISABLE POPUP EVERYWHERE
   
   timing: {
     showAfterMs: 1000,       // Show popup 2 seconds after load
